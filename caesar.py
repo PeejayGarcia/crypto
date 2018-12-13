2f058098661d6a05f3e4101708cbde0d03050e10
Peejay Garcia <peejgarcia@gmail.com>
2018-12-12 22:10:12 -0600

    Created function

---
+++ caesar.py
@@ -0,0 +1,5 @@
+#function should be case sensitive
+#You can assume that your input will definitely be a letter. Don’t worry about what might 
+#happen if somebody tries to use your function with an input parameter that is something 
+# other than a single letter, like alphabet_position("grandpa22!")
+def alphabet_position(letter):
