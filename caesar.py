5c92e4e76b49c27957b0082655e4d1d76ae12eef
Peejay Garcia <peejgarcia@gmail.com>
2018-12-20 18:52:52 -0600

    Update caesar.py

--- caesar.py
+++ caesar.py
@@ -2,33 +2,82 @@
 #You can assume that your input will definitely be a letter. Don’t worry about what might 
 #happen if somebody tries to use your function with an input parameter that is something 
 # other than a single letter, like alphabet_position("grandpa22!")
-import string
     
 ####
 #      use alphabet_position maybe
 #      preserve case
+import string
 
-def rotate_character(char, rot)
+def rotate_character(char, rot):
     lower = string.ascii_lowercase
     upper = string.ascii_uppercase
+
+#   while char.isalpha():
+#         if char.isupper():
+#             blah
+#             blah
+#             blah
+            
+#             return new_char
+#         else:
+#             blah
+#             blah
+#             blah
+            
+#             return new_char
+    
+#     return char
+
+    while char.isalpha:
+        if char.isupper == True:
+            return char
+        if char.islower == True:
+            return char
+        elif char.isalpha != True:
+            return char, alphabet_position(char)
+
+
+    
+def alphabet_position(letter):
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    low_letter = letter.lower()
+    for char in low_letter:
+        if char == ' ':
+            letter_pos = letter_pos + ' '
+
+        else:
+            letter_pos = alphabet.index(char)
+    
+    return letter_pos
+
+
+def main():
+    print(rotate_character('b', 0))
+          
+if __name__ == "__main__":
+          main()
+
     #char = alphabet_position
 
     #while statement, isalpha
     #if character is upper he did index =
 
-    while char.isalpha:
-#        char = alphabet_position(char)
-        if char.isupper == True:
-            return char
+#     for i in char:
+# #        char = alphabet_position(char)
+#         if char.isupper == True:
+#             return char
+#         if char.islower == True:
+#             return char
+#         if char.isalpha() == True:
+#             return char
+#         else:
+#             return char
 
-        if char.islower == True:
-            return char
-    
-    
-    
-
-    return rotated 
-
+def main():
+    print(rotate_character('!',0))
+          
+if __name__ == "__main__":
+          main()
 
 ####
 
