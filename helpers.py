38c6a63790af1d6a5334b2847cb3d989939552db
Peejay Garcia <peejgarcia@gmail.com>
2018-12-21 23:39:57 -0600

    Create helpers.py

---
+++ helpers.py
@@ -0,0 +1,33 @@
+import string
+
+
+def alphabet_position(letter):
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    while letter.isalpha():
+        if letter.isupper():
+            lower = letter.lower()
+            letter_pos = alphabet.index(lower)
+            return letter_pos
+        if letter == ' ':
+            letter_pos = letter_pos + ' '
+            return letter_pos
+        else:
+            letter_pos = alphabet.index(letter)
+            return letter_pos
+    return letter
+
+
+def rotate_character(char, rot):
+    lower = string.ascii_lowercase
+    upper = string.ascii_uppercase
+
+    while char.isalpha():
+        if char.isupper():
+            position = ((upper.find(char) + rot) % 26)
+            new_position = upper[position]
+            return new_position
+        else:
+            position = ((lower.find(char)  + rot) % 26)
+            new_position = lower[position]
+            return new_position  
+    return char
