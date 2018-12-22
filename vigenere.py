f212c980106dea042618bcf3feba25cc27a6c7c3
Peejay Garcia <peejgarcia@gmail.com>
2018-12-21 23:12:42 -0600

    Update vigenere.py

--- vigenere.py
+++ vigenere.py
@@ -34,11 +34,25 @@ def rotate_character(char, rot):
 
 
 def encrypt(text, key):
+    encrypted = ''
+    counter = 0
+    i = 0
+    for letter in text:
+        if letter.isalpha():
+            i = alphabet_position(key[counter])
+            encrypted = encrypted + rotate_character(letter, i)
+            counter = ((counter + 1) % len(key))
+        else:
+            encrypted = encrypted + letter
 
-
+    return encrypted
 
 def main():
-
+    text = input("Type a message: ")
+    print(text)
+    key = input("Encryption key: ")
+    print(key)
+    print(encrypt(text, key))
           
 if __name__ == "__main__":
           main()
